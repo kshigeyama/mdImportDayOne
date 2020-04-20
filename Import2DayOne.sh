cf4d0ca9240b05737515bede7942defa8dbd2383
@kshigeyama <kshigeyama@dreampresenter.info>
2020-04-20 12:27:06 +0900

    修正！？

--- Import2DayOne.sh
+++ Import2DayOne.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 # 定義　適宜変えてね
-#JOURNALNAME="AppleHealthCare"
-#JOURNALNAME="ジャーナル"
-#TAG="ヘルスケア"
+# JOURNALNAME="AppleHealthCare"
+# TAG="ヘルスケア"
 DIR=./mdfiles
-
+FILEEXT="md"
 
 # 問題ない限りは、以下は変更しないでね -maxdepth 1 
 DAYONE_APP_PATH="/Applications/Day One.app"
-for i in `find $DIR -name "*.md"`; do
+for i in `find $DIR -name "*.$FILEEXT"`; do
 
 filename=${i##*/}
 YEAR=${filename:0:4}
 MONTH=${filename:5:2}
 DAY=${filename:8:2}
 ENTRYDATE=$YEAR-$MONTH-$DAY
-#-j ${JOURNALNAME} -t ${TAG}  --tag "${TAG}"    
-dayone2 --date ${ENTRYDATE}  new <  ${i} 
+#-j ${JOURNALNAME} -t ${TAG}  --tag "${TAG}"
+dayone2 --date ${ENTRYDATE} new <  ${i} 
 
 done
--- README.md
+++ README.md
@@ -3,6 +3,9 @@ MarkDown ( and other text files ) import DayOne Journal bash Script
 このファイルは、Markdown形式ファイルやちょっと手を加えるとその他テキストファイルをDayOneのジャーナルに取り込むことができるシェルスクリプトです。
 
 # How to use
+## ファイル名の付け方と日付
+ファイル名は YYYY-MM-DD-freetext.md などとなり、YYYY-MM-DD の日付にインポートします。
+
 ## ファイルのインポート方法
 
 * Import2DayOne.sh を取得
@@ -17,3 +20,4 @@ EXT="*.md" の部分の md を他の拡張子にしてください。
 
 # 制限事項
 * macOSでのみ動作確認していません。
+* dayone2 の挙動が解らず、tagとジャーナル指定でエラーが発生します。対処法ご存じの方は教えてください。
